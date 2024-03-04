28bd35078cd840e8b64ff174b82c9aeb60425c64
Mohammadreza Jafari <134132787+jafaryi@users.noreply.github.com>
2024-03-04 10:21:43 +0330

    add new file for create video from images
    add new file for create video from images

---
+++ creat-video.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+
+def create_video_from_images(image_folder, video_name, fps=30):
+    images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
+
+    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+
+    cv2.destroyAllWindows()
+    video.release()
+
+# مسیر فولدر عکسها
+image_folder = 'C:\\Users\\Mohammadreza\\Desktop\\Newfolder\\video-data'
+# نام ویدیو خروجی
+video_name = 'C:\\Users\\Mohammadreza\\Desktop\\Newfolder\\testVideo.mp4'
+
+# ساخت ویدیو
+create_video_from_images(image_folder, video_name)
