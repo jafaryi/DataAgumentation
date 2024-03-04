8d3aa6cdbae6d116e763550ff6423f97edf2ac9b
Mohammadreza Jafari <134132787+jafaryi@users.noreply.github.com>
2024-03-04 10:22:52 +0330

    code for rename labels of images
    code for rename labels of images

---
+++ rename.py
@@ -0,0 +1,38 @@
+import os
+
+# مسیر فولدر مبدأ
+source_folder = 'C:\\Users\\Mohammadreza\\Desktop\\Newfolder\\ali-file\\labels'
+
+# مسیر فولدر مقصد
+destination_folder = 'C:\\Users\\Mohammadreza\\Desktop\\Newfolder\\ali-file\\labels1'
+
+# ایجاد فولدر مقصد
+os.makedirs(destination_folder, exist_ok=True)
+
+# لیست تمام فایل‌ها در فولدر مبدأ
+files = os.listdir(source_folder)
+
+# شماره شروع نامگذاری
+start_index = 0
+
+# تغییر نام فایل‌ها و انتقال به فولدر مقصد
+for file in files:
+    # جدا کردن شماره فریم از نام اولیه
+    _, frame_number = os.path.splitext(file)
+    
+    # حاصل جمع شماره شروع و شماره فریم
+    new_name = f'frame_{start_index:03d}{frame_number}'
+    
+    # مسیر فایل مبدأ
+    source_path = os.path.join(source_folder, file)
+    
+    # مسیر فایل مقصد
+    destination_path = os.path.join(destination_folder, new_name)
+    
+    # انتقال و تغییر نام فایل
+    os.rename(source_path, destination_path)
+    
+    # افزایش شماره شروع برای فایل بعدی
+    start_index += 1
+
+print('تغییر نام و انتقال فایل‌ها انجام شد.')
